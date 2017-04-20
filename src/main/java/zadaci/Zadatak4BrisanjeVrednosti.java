4ed166b966a690abc98240c71ca87b6346d239f3
borisbokan <borisbokan@gmail.com>
2017-04-20 19:09:13 +0200

    Uradjena Gradle

---
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -0,0 +1,11 @@
+package zadaci;
+
+/**
+ * Created by androiddevelopment on 20.4.17..
+ */
+public class Zadatak4BrisanjeVrednosti {
+
+        public static void main(String[] args){
+
+        }
+}
---
+++ src/main/java/zadaci/baza.java
@@ -0,0 +1,41 @@
+package zadaci;
+
+
+import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+/**
+ * Created by androiddevelopment on 20.4.17..
+ */
+public class baza {
+
+            private ConnectionSource conn=null;
+
+
+            public baza(){
+                try {
+                    conn=new JdbcConnectionSource("jdbc:sqlite:knjigaOblast.db");
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+            public ConnectionSource konekcija(){
+                return conn;
+            }
+
+
+            public void zatvoriKonekciju(){
+                try {
+                    conn.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+}
